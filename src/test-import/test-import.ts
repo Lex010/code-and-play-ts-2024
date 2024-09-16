8be38f1efc24624283c6bf2b4a43f2606251366c
Lex Mel <lex07672@gmail.com>
2024-09-16 21:04:33 +0300

    feat: add json file for en and ru data, test it

--- src/test-import/test-import.ts
+++ src/test-import/test-import.ts
@@ -1,6 +1,18 @@
+import dataRu from '../data/ru.json';
+import dataEn from '../data/en.json';
+
+let currentLanguage = 'ru'; // или 'en' в зависимости от начального языка
+
 export default function test() {
   const div = document.createElement('div');
-  div.textContent = 'тест';
+  div.textContent = dataRu.name;
   div.style.color = 'white';
   document.body.appendChild(div);
+
+  div.style.cursor = 'pointer'; // Добавляем указатель, чтобы показать, что элемент кликабелен
+  // Обработчик клика для переключения языка
+  div.addEventListener('click', () => {
+    currentLanguage = currentLanguage === 'ru' ? 'en' : 'ru';
+    div.textContent = currentLanguage === 'ru' ? dataRu.name : dataEn.name;
+  });
 }
